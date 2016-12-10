230ff2f6a87ca2942a8f03963dfdd83c9e5a9af1
vagrant <vagrant@thdengops-ubuntu-14.04-virtualbox>
2016-12-10 19:19:15 +0000

    Proyecto creado

---
+++ actions/randomNumber.js
@@ -0,0 +1,15 @@
+'use strict'
+
+exports.randomNumber = {
+  name: 'randomNumber',
+  description: 'I am an API method which will generate a random number',
+  outputExample: {
+    randomNumber: 0.123
+  },
+
+  run: function(api, data, next){
+    data.response.randomNumber = Math.random();
+    next();
+  }
+
+};
